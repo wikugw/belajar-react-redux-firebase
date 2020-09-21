0b5eea8bc27ae0aca32ab4ea19f37be4bc8ee0c8
wikugw <wikugw@gmail.com>
2020-09-21 10:37:28 +0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
+import CreateProject from './components/projects/CreateProject';
 import ProjectDetails from './components/projects/ProjectDetails';
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/create" component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -5,6 +5,9 @@ const ProjectList = () => {
   return (
     <div className="project-list section">
       <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   )
 }
