88102c65860a1bde54fd8baf5946debeca618c04
wikugw <wikugw@gmail.com>
2020-09-23 08:59:43 +0700

    sign up

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,3 +1,5 @@
+import { getFirebase } from "react-redux-firebase";
+
 export const signIn = (credentials) => {
   return (dispatch, getState, { getFirebase }) => {
     const firebase = getFirebase();
@@ -18,5 +20,27 @@ export const signOut = () => {
     firebase.auth().signOut().then(() => {
       dispatch({ type: 'SIGNOUT_SUCCESS' })
     });
+  }
+}
+
+export const signUp = (newUser) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+
+    firebase.auth().createUserWithEmailAndPassword(
+      newUser.email,
+      newUser.password
+    ).then((response) => {
+      return firestore.collection('users').doc(response.user.uid).set({
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        initials: newUser.firstName[0] + newUser.lastName[0]
+      })
+    }).then(() => {
+      dispatch({ type: 'SIGNUP_SUCCESS' })
+    }).catch((error) => {
+      dispatch({ type: 'SIGNUP_ERROR', error })
+    })
   }
 }
